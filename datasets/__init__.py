b7cc77b44a30b1cb5d75a7132e30ecddb51d7af9
qrzyang <134265735+qrzyang@users.noreply.github.com>
2024-11-18 21:43:54 +0800

    Update for a init bug

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,7 +1,5 @@
 from .kitti_y import KITTIDataset, KITTI_Raw
-from .MiddEval import MiddEval
 __datasets__ = {
     "kitti": KITTIDataset, 
-    "kitti_raw": KITTI_Raw,
-    "MiddEval": MiddEval
+    "kitti_raw": KITTI_Raw
 }
